8b091662068bfff028cba598ba1eef4a585df023
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-11-06 16:43:13 +0100

    contact list

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { removeContact } from 'globalState/slices/constatsSlice';
+
+const ContactList = ({ visibleList }) => {
+  console.log(visibleList);
+  const dispatch = useDispatch();
+
+  return (
+    <ul>
+      {visibleList.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <a href={`tel:${number}`}>
+              {name}: <span>{number}</span>
+            </a>
+            <button key={id} onClick={() => dispatch(removeContact(id))}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default ContactList;

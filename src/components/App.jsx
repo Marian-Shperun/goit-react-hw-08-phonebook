12ddd13e8333d9233ed348ef38a5e826563d73a1
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-11-06 16:41:28 +0100

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { useSelector } from 'react-redux';
+import { getContacts } from '../globalState/slices/constatsSlice';
+import { getFilterStatus } from '../globalState/slices/filterSlice';
+
+import ContactForm from './ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList';
+
 export const App = () => {
+  const contacts = Object.values(useSelector(getContacts));
+  const filter = useSelector(getFilterStatus);
+
+  const filteredContacts = () => {
+    const normilizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact?.name?.toLowerCase()?.includes(normilizeFilter)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React Template
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} />
+
+      <h2>Contacts</h2>
+      {contacts.length === 0 ? (
+        <p>No contacts added</p>
+      ) : (
+        <>
+          <Filter title="Find contacts by name" contacts={filteredContacts} />
+          <ContactList
+            visibleList={filter !== '' ? filteredContacts() : contacts}
+          />
+        </>
+      )}
     </div>
   );
 };

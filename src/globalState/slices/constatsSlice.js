8eea3308fc7dcc7dd0da41effc44ba50d801c9b9
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-11-06 17:30:31 +0100

    adds  persistReducer

--- src/globalState/slices/constatsSlice.js
+++ src/globalState/slices/constatsSlice.js
@@ -1,29 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-
-const contacts = [];
+const contactsinitialState = {
+  contacts: [],
+};
 
 export const contactsSlice = createSlice({
   // 1 робить action (дія)
   name: 'contacts',
-  initialState: contacts,
+  initialState: contactsinitialState,
   // 2 робить reduser щоб обробляти action
   reducers: {
     addContact(state, action) {
-      state.push(action.payload);
+      state.contacts.push(action.payload);
     },
     removeContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      const index = state.contacts.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.contacts.splice(index, 1);
     },
   },
 });
 
+const persisteConfig = {
+  key: 'contacts',
+  storage,
+};
 
-
-export const getContacts = state => state.contacts;
+export const getContacts = state => state.contacts.contacts;
 
 export const { addContact, removeContact } = contactsSlice.actions;
 
-export const contactsReducer = contactsSlice.reducer;
-
+export const contactsReducer = persistReducer(
+  persisteConfig,
+  contactsSlice.reducer
+);

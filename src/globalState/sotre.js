a68e8c67109af29fde810b30afd7b2a81f1bcb00
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-11-20 17:49:39 +0100

    adds persist and auth reducer

--- src/globalState/sotre.js
+++ src/globalState/sotre.js
@@ -1,11 +1,42 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import { contactsReducer } from './slices/contacts/constatsSlice';
-import { filterReducer } from './slices/filterSlice';
+import { contactsReducer } from './contacts/constatsSlice';
+import { filterReducer } from './contacts/filterSlice';
+import { authReducer } from './auth/authSlice';
+
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+
+
+export const persistor = persistStore(store);

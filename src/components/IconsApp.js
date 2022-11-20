79f0d4d2d75d1cc23db7703deae947c041744ac1
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-11-20 17:55:43 +0100

    adds icons for app

---
+++ src/components/IconsApp.js
@@ -0,0 +1,35 @@
+// UserMenu
+import LogoutIcon from '@mui/icons-material/Logout';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
+// FormAuth
+import PersonIcon from '@mui/icons-material/Person';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+
+// FormContact
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+
+// ContactList
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+// Filter
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+
+// BtnAddNewContact
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+
+
+export {
+  LogoutIcon,
+  ExpandMoreIcon,
+  ExpandLessIcon,
+  AccountCircleIcon,
+  PersonSearchIcon,
+  PersonIcon,
+  AlternateEmailIcon,
+  VpnKeyIcon,
+  LocalPhoneIcon,
+  PersonAddIcon,
+};

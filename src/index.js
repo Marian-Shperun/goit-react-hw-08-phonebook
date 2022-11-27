bd02ab257d27fe266161a01358a5c910f7e71603
Marian Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-11-27 19:20:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <App />
       </BrowserRouter>
     </PersistGate>
